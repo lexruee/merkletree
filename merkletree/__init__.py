4eabe7b1236b00233124647394b1a6ba4e209bd4
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-11-20 13:55:31 +0100

    first commit: add naive merkle tree impl

---
+++ merkletree/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Alexander Rüedlinger"
+
+__all__ = ('Tree', 'Node')
+
+from .merkletree import Node
+from .merkletree import PrintVisitor
---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Alexander Rüedlinger"
+
+from setuptools import setup, find_packages
+
+setup(
+    name='merkletree',
+    version='0.1',
+    packages=find_packages(),
+)
